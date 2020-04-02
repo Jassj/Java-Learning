4b77ab6bbfe941eab976e07f5e1d602fa08bf099
yuanjie <172161401@qq.com>
2020-04-02 21:52:42 +0800

    DynamicLinking

--- src/main/java/jvm/DynamicLinkingTest.java
+++ src/main/java/jvm/DynamicLinkingTest.java
@@ -6,14 +6,52 @@ package jvm;
  * @author yuanjie 2020/03/31 23:06
  */
 public class DynamicLinkingTest {
-
-    public void methodA() {
-
+    public void showAnimal(Animal animal) {
+        animal.eat(); // 晚期绑定：存在继承的子类, 需要具体实现类调用
     }
 
-    public void methodB() {
-        System.out.println("methodB()....");
+    public void showHunt(Hunting h) {
+        h.hunt(); // 晚期绑定：接口不可以实例化, 需要具体实现类调用
+    }
+}
 
-        methodA();
+interface Hunting {
+    void hunt();
+}
+
+class Animal {
+    public void eat() {
+        System.out.println("动物觅食");
+    }
+}
+
+class Cat extends Animal implements Hunting{
+
+    public Cat() { // 早期绑定: 指定调用父类构造方法
+        super();
+    }
+
+    public Cat(String name) { // 早期绑定: 指定本类构造方法
+        this();
+    }
+
+    public void eat() {
+        System.out.println("猫吃鱼");
+    }
+
+    @Override
+    public void hunt() {
+        System.out.println("猫抓老鼠");
+    }
+}
+
+class Fish extends Animal implements Hunting{
+    public void eat() {
+        System.out.println("鱼吃虾米");
+    }
+
+    @Override
+    public void hunt() {
+        System.out.println("大鱼吃小鱼");
     }
 }
