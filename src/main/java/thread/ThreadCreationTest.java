7e681beb3d340e5209d544bf295b4d0df0cdbab0
yuanjie <172161401@qq.com>
2020-05-10 21:38:11 +0800

    create Thread

--- src/main/java/thread/ThreadCreationTest.java
+++ src/main/java/thread/ThreadCreationTest.java
@@ -1,45 +1,8 @@
 package thread;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.FutureTask;
-
-class RunnableDemo implements Runnable{
-    private Thread thread;
-    private String threadName;
-
-    public RunnableDemo(String threadName) {
-        this.threadName = threadName;
-        System.out.println("Creating " + threadName);
-    }
-
-    @Override
-    public void run() { // 线程调度器将就绪线程设置为运行线程, 此时线程处于运行状态, 将会调用run()方法
-        System.out.println("Running " + threadName);
-        try {
-            for(int i = 0; i < 5; i++) {
-                System.out.println("Thread: " + threadName + ", " + i);
-                //Sleep
-                Thread.sleep(50);
-            }
-        } catch (Exception e) {
-            System.out.println("Thread " +  threadName + " interrupted.");
-        }
-        System.out.println("Thread " +  threadName + " exiting.");
-    }
-
-    public void start() {
-        System.out.println("Starting  " + threadName);
-
-        if(thread == null) {
-            thread = new Thread(this, threadName);
-            thread.start(); // 线程进入就绪状态, 等待线程调度器
-        }
-    }
-}
+import java.util.concurrent.*;
 
 class MyThread extends Thread {
-    private Thread thread;
     private String threadName;
 
     public MyThread(String threadName) {
@@ -53,15 +16,29 @@ class MyThread extends Thread {
         System.out.println("Thread " +  threadName + " exiting.");
     }
 
-    public void start() {
-        System.out.println("Starting  " + threadName);
+}
 
-        if(thread == null) {
-            thread = new Thread(this, threadName);
-            thread.start();
-        }
+class RunnableDemo implements Runnable {
+    private String threadName;
+
+    public RunnableDemo(String threadName) {
+        this.threadName = threadName;
+        System.out.println("Creating " + threadName);
     }
 
+    @Override
+    public void run() { // 线程调度器将就绪线程设置为运行线程, 此时线程处于运行状态, 将会调用run()方法
+        System.out.println("Running " + threadName);
+        try {
+            for(int i = 0; i < 5; i++) {
+                System.out.println("Thread: " + threadName + ", " + i);
+                Thread.sleep(50);
+            }
+        } catch (Exception e) {
+            System.out.println("Thread " +  threadName + " interrupted.");
+        }
+        System.out.println("Thread " +  threadName + " exiting.");
+    }
 }
 
 class CallableThreadTest implements Callable<Integer> {
@@ -83,15 +60,6 @@ class CallableThreadTest implements Callable<Integer> {
 }
 
 public class ThreadCreationTest {
-    // 通过实现runnable接口创建线程
-    public static void runnableThreadTest() {
-        RunnableDemo r1 = new RunnableDemo("Thread-1");
-        r1.start();
-
-        RunnableDemo r2 = new RunnableDemo("Thread-2");
-        r2.start();
-    }
-
     // 通过继承线程类创建线程
     public static void threadExtendTest() {
         MyThread t1 = new MyThread("Thread-1");
@@ -100,27 +68,32 @@ public class ThreadCreationTest {
         t2.start();
     }
 
-    // 通过实现callable创建线程
+    // 通过实现runnable接口创建线程
+    public static void runnableThreadTest() {
+        RunnableDemo r1 = new RunnableDemo("Thread-1");
+        new Thread(r1).start();
+
+        RunnableDemo r2 = new RunnableDemo("Thread-2");
+        new Thread(r2).start();
+    }
+
+    // 通过 Callable & Future 创建线程
     public static void callableThreadTest() throws ExecutionException, InterruptedException {
         CallableThreadTest c = new CallableThreadTest();
-        FutureTask<Integer> f = new FutureTask<>(c);
-//        Thread t0 = new Thread(f, "Thread-0");
-//        t0.start();
-//        System.out.println(f.get());
-//        Thread t1 = new Thread(f, "Thread-1");
-//        t1.start();
-//        System.out.println(f.get());
-//        Thread t2 = new Thread(f, "Thread-2");
-//        t2.start();
-//        System.out.println(f.get());
-        Thread t3 = new Thread(f, "Thread-3");
-        t3.start();
-        System.out.println(f.get());
+        FutureTask<Integer> f1 = new FutureTask<>(c);
+        FutureTask<Integer> f2 = new FutureTask<>(c);
+        Thread t1 = new Thread(f1, "Thread-1");
+        t1.start();
+        System.out.println(f1.get());
+        Thread t2 = new Thread(f2, "Thread-2");
+        t2.start();
+        System.out.println(f2.get());
     }
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-//        runnableThreadTest();
 //        threadExtendTest();
+//        runnableThreadTest();
         callableThreadTest();
     }
+
 }
